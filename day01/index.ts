e6fa2bdaadddeb1c381af294334a3669332ce95b
Luke Kuzmish <lakuzmish@gmail.com>
2025-12-06 09:06:01 -0500

    part 2 not working

--- day01/index.ts
+++ day01/index.ts
@@ -1,5 +1,5 @@
 const FILE_PATH: string = "puzzle_input.txt";
-const USE_SAMPLE = false;
+const USE_SAMPLE = true;
 const SAMPLE_TEXT = `L68
 L30
 R48
@@ -22,11 +22,12 @@ async function getInput(): Promise<number[]> {
   str = str.replaceAll("L", "-").replaceAll("R", "+");
 
   const lines: string[] = str.split("\n");
-  return lines.map((value) => parseInt(value, 10));
+  return lines
+    .filter((value) => value !== "")
+    .map((value) => parseInt(value, 10));
 }
 
-function processInput(input: number[]): number {
-  console.log(input);
+function processInputPart1(input: number[]): number {
   let timesAtZero = 0;
   let currentPosition = 50;
 
@@ -45,6 +46,32 @@ function processInput(input: number[]): number {
 
   return timesAtZero;
 }
+function processInputPart2(input: number[]): number {
+  let timesPassingZero: number = 0;
+  let currentPosition = 50;
+
+  input.forEach(function (toMove: number) {
+    let temp = Math.floor(Math.abs(toMove) / 100);
+    console.log(`toMove: ${toMove} temp: ${temp}`);
+    timesPassingZero += temp;
+    toMove = toMove % 100;
+    currentPosition += toMove;
+    if (currentPosition >= 100) {
+      timesPassingZero++;
+      currentPosition -= 100;
+    } else if (currentPosition < 0) {
+      currentPosition = 100 + currentPosition;
+      timesPassingZero++;
+    }
+    if (currentPosition === 0) {
+      //timesPassingZero++;
+    }
+  });
+
+  console.log("Final position: " + currentPosition);
+  return timesPassingZero;
+}
 
 const input = await getInput();
-console.log("times at zero: " + processInput(input));
+//console.log("times at zero part1: " + processInputPart1(input));
+console.log("times passing zero: " + processInputPart2(input));
